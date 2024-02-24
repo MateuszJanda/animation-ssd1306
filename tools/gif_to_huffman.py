df201c115eb1ff70b487fb015ec95be3b0c247ae
Mateusz Janda <mateusz.janda@gmail.com>
2024-02-24 21:46:05 +0100

    Refactor

--- tools/gif_to_huffman.py
+++ tools/gif_to_huffman.py
@@ -8,14 +8,9 @@ import numpy as np
 GIF_NAME = "skull.gif"
 RESIZE_FACTOR = 0.5
 
-
-def main() -> None:
-    # output = subprocess.run(
-    #     ["ffmpeg", "-r", "1", "-i", f"{GIF_NAME}", "-r", " 1", r"%04d.bmp"],
-    #     stdout=subprocess.PIPE,
-    # )
-
-    image = cv2.imread("0001.bmp")
+def resize_image(file_name: str) -> np.ndarray:
+    """Resize image from 200x200 to 128x64."""
+    image = cv2.imread(file_name)
     print(f"Height x Width x Channels: {image.shape}, dtype: {image.dtype}")
 
     small_image = cv2.resize(image, (0, 0), fx=RESIZE_FACTOR, fy=RESIZE_FACTOR)
@@ -27,6 +22,18 @@ def main() -> None:
 
     cv2.imshow("image_window", output_image)
     cv2.waitKey(0)
+
+    return output_image
+
+
+def main() -> None:
+    # output = subprocess.run(
+    #     ["ffmpeg", "-r", "1", "-i", f"{GIF_NAME}", "-r", " 1", r"%04d.bmp"],
+    #     stdout=subprocess.PIPE,
+    # )
+
+    resize_image("0001.bmp")
+
 
 
 if __name__ == "__main__":
