0eabdb4587a85ab67ec35a7bbcae4ebe6fcd95a6
Mateusz Janda <mateusz.janda@gmail.com>
2024-05-04 22:48:11 +0200

    Refactor HuffmanCodeDecoder

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use ssd1306::{prelude::*, Ssd1306};
 
 const ARRAY_CHUNK_SIZE: usize = 128;
 
+/// Huffman code decoder.
 struct HuffmanFrameDecoder<DI, SIZE>
 where
     DI: WriteOnlyDataCommand,
@@ -26,120 +27,82 @@ where
     DI: WriteOnlyDataCommand,
     SIZE: DisplaySize,
 {
+    /// Create instance of Huffman code decoder.
     pub fn new(display: Ssd1306DriverWrapper<DI, SIZE>) -> Self {
         Self { display }
     }
 
+    /// Decode frame_chunk and draw it on display.
     pub fn decode(&mut self, frame_bits_size: usize, frame_array: &ProgMem<[u8; 384]>) {
-        // let frame_bits_size = SKULL_FRAME02_BITS_SIZE.load();
-        // let x = BINARY_TREE_LEAFS.load_sub_array::<128>(0);
-        let mut bt_start = 0;
-        let mut bt = BINARY_TREE_LEAFS.load_sub_array::<ARRAY_CHUNK_SIZE>(bt_start);
+        let mut bt_chunk_start = 0;
+        let mut bt_chunk = BINARY_TREE_LEAFS.load_sub_array::<ARRAY_CHUNK_SIZE>(bt_chunk_start);
 
-        let mut frame_start = 0;
-        let mut frame = frame_array.load_sub_array::<ARRAY_CHUNK_SIZE>(frame_start);
-        let mut current_index = 1;
-        let mut current_code = 0;
+        let mut frame_chunk_start = 0;
+        let mut frame_chunk = frame_array.load_sub_array::<ARRAY_CHUNK_SIZE>(frame_chunk_start);
 
-        // let mut value_start = 0;
-        // let mut value: [u8; 128] = BINARY_TREE_INDEXES_TO_VALUES.load_sub_array::<128>(value_start);
+        let mut frame_bit_index = 1;
+        let mut huffman_code = 0;
 
-        let mut buf = [0; ARRAY_CHUNK_SIZE];
-        let mut buf_i = 0;
+        let mut buffer = [0; ARRAY_CHUNK_SIZE];
+        let mut buffer_byte_count = 0;
+
         for i in 0..frame_bits_size {
-            // ufmt::uwriteln!(&mut serial, "BUKA i: {}, buf_i: {}, current_index: {}", i, buf_i, current_index).unwrap();
-
-            let frame_byte = i / 8;
-            // let frame_bit = i % 8;
-
-            if frame_byte - frame_start >= ARRAY_CHUNK_SIZE {
-                frame_start = (frame_byte / ARRAY_CHUNK_SIZE) * ARRAY_CHUNK_SIZE;
-                frame = frame_array.load_sub_array::<ARRAY_CHUNK_SIZE>(frame_start);
+            let frame_byte_index = i / 8;
+            // If frame byte is not in current chunk, read proper one.
+            if frame_byte_index - frame_chunk_start >= ARRAY_CHUNK_SIZE {
+                frame_chunk_start = (frame_byte_index / ARRAY_CHUNK_SIZE) * ARRAY_CHUNK_SIZE;
+                frame_chunk = frame_array.load_sub_array::<ARRAY_CHUNK_SIZE>(frame_chunk_start);
             }
 
-            let sub_frame_byte = frame_byte % ARRAY_CHUNK_SIZE;
-            let sub_frame_bit = i % 8;
+            let frame_chunk_byte_index = frame_byte_index % ARRAY_CHUNK_SIZE;
+            let frame_chunk_bit_index = i % 8;
 
-            if frame[sub_frame_byte] & (0b1000_0000 >> sub_frame_bit) != 0 {
-                current_index = 2 * current_index + 1;
-                current_code = current_code << 1 | 1;
+            // For "1" choose right branch, for "0" choose left branch.
+            if frame_chunk[frame_chunk_byte_index] & (0b1000_0000 >> frame_chunk_bit_index) != 0 {
+                frame_bit_index = 2 * frame_bit_index + 1;
+                huffman_code = huffman_code << 1 | 1;
             } else {
-                current_index = 2 * current_index;
-                current_code = current_code << 1;
+                frame_bit_index = 2 * frame_bit_index;
+                huffman_code = huffman_code << 1;
             }
-            // ufmt::uwriteln!(&mut serial, "BUKA current_index {}", current_index).unwrap();
-            // ufmt::uwriteln!(
-            //     &mut serial,
-            //     "BUKA i: {}, buf_i: {}, current_index: {}, current_code: {}, bit: {}",
-            //     i,
-            //     buf_i,
-            //     current_index,
-            //     current_code,
-            //     frame[sub_frame_byte] & (0b1000_0000 >> sub_frame_bit) != 0
-            // )
-            // .unwrap();
 
-            let bt_byte = current_index / 8;
-
-            // ufmt::uwriteln!(
-            //     &mut serial,
-            //     "BUKA b bt_byte:{} bt_start:{}",
-            //     bt_byte,
-            //     bt_start
-            // )
-            // .unwrap();
-            // serial.flush();
-
-            if bt_byte < bt_start || bt_byte - bt_start >= ARRAY_CHUNK_SIZE {
-                bt_start = (bt_byte / ARRAY_CHUNK_SIZE) * ARRAY_CHUNK_SIZE;
-                bt = BINARY_TREE_LEAFS.load_sub_array::<ARRAY_CHUNK_SIZE>(bt_start);
+            let bt_byte_index = frame_bit_index / 8;
+            // If binary tree (bt) byte in not in current chunk, read proper one.
+            if bt_byte_index < bt_chunk_start || bt_byte_index - bt_chunk_start >= ARRAY_CHUNK_SIZE
+            {
+                bt_chunk_start = (bt_byte_index / ARRAY_CHUNK_SIZE) * ARRAY_CHUNK_SIZE;
+                bt_chunk = BINARY_TREE_LEAFS.load_sub_array::<ARRAY_CHUNK_SIZE>(bt_chunk_start);
             }
-            let sub_bt_byte = bt_byte % ARRAY_CHUNK_SIZE;
-            let sub_bt_bit = current_index % 8;
 
-            if bt[sub_bt_byte] & (0b1000_0000 >> sub_bt_bit) != 0 {
-                // is leaf
+            let bt_chunk_byte_index = bt_byte_index % ARRAY_CHUNK_SIZE;
+            let bt_chunk_bit_index = frame_bit_index % 8;
 
+            // Check if is a leaf (marked as bit 1) in binary tree.
+            if bt_chunk[bt_chunk_byte_index] & (0b1000_0000 >> bt_chunk_bit_index) != 0 {
                 let mut lo: usize = 0;
                 let mut hi: usize = BINARY_TREE_CODES.len() - 1;
                 let mut mi: usize = (hi - lo) / 2 + lo;
 
-                // let search_code = current_index - 2;
-                // let search_code = current_code;
-                // ufmt::uwriteln!(&mut serial, "BUKA current_code 0x{:04x}", current_code).unwrap();
                 while lo <= hi {
                     mi = (hi - lo) / 2 + lo;
-                    // ufmt::uwriteln!(&mut serial, "BUKA  mi: {}", mi).unwrap();
-
-                    if current_code == BINARY_TREE_CODES[mi] as usize {
-                        // ufmt::uwriteln!(&mut serial, "BUKA break mi: {}", mi).unwrap();
+                    if huffman_code == BINARY_TREE_CODES[mi] as usize {
                         break;
-                    } else if current_code < BINARY_TREE_CODES[mi] as usize {
+                    } else if huffman_code < BINARY_TREE_CODES[mi] as usize {
                         hi = mi - 1;
                     } else {
                         lo = mi + 1;
                     }
                 }
-                // ufmt::uwriteln!(&mut serial, "BUKA  mi: {}", mi).unwrap();
 
-                // let mut mi_byte: usize = mi;
-                // if mi_byte < value_start || mi_byte - value_start >= 128 {
-                //     value_start = (mi_byte / 128) * 128;
-                //     value = BINARY_TREE_INDEXES_TO_VALUES.load_sub_array::<128>(value_start);
-                //     mi_byte = mi_byte % 128;
-                // }
+                buffer[buffer_byte_count] = BINARY_TREE_VALUES[mi];
+                frame_bit_index = 1;
+                huffman_code = 0;
+                buffer_byte_count += 1;
 
-                buf[buf_i] = BINARY_TREE_VALUES[mi];
-                // ufmt::uwriteln!(&mut serial, "BUKA mi {}, value 0x{:x} ", mi, buf[buf_i]).unwrap();
-                current_index = 1;
-                current_code = 0;
-
-                // let mi_byte =BINARY_TREE_INDEXES_TO_VALUES
-
-                buf_i += 1;
-                if buf_i == buf.len() {
-                    self.display.draw_strips_from_buffer(&buf).unwrap();
-                    buf_i = 0;
+                // Flush buffer if is full
+                if buffer_byte_count == buffer.len() {
+                    self.display.draw_strips_from_buffer(&buffer).unwrap();
+                    buffer_byte_count = 0;
                 }
             }
         }
@@ -207,7 +170,6 @@ fn main() -> ! {
             7 => (SKULL_FRAME07_BITS_SIZE.load(), &SKULL_FRAME07),
             8 => (SKULL_FRAME08_BITS_SIZE.load(), &SKULL_FRAME08),
             9 => (SKULL_FRAME09_BITS_SIZE.load(), &SKULL_FRAME09),
-
             10 => (SKULL_FRAME10_BITS_SIZE.load(), &SKULL_FRAME10),
             11 => (SKULL_FRAME11_BITS_SIZE.load(), &SKULL_FRAME11),
             12 => (SKULL_FRAME12_BITS_SIZE.load(), &SKULL_FRAME12),
@@ -218,7 +180,6 @@ fn main() -> ! {
             17 => (SKULL_FRAME17_BITS_SIZE.load(), &SKULL_FRAME17),
             18 => (SKULL_FRAME18_BITS_SIZE.load(), &SKULL_FRAME18),
             19 => (SKULL_FRAME19_BITS_SIZE.load(), &SKULL_FRAME19),
-
             20 => (SKULL_FRAME20_BITS_SIZE.load(), &SKULL_FRAME20),
             21 => (SKULL_FRAME21_BITS_SIZE.load(), &SKULL_FRAME21),
             22 => (SKULL_FRAME22_BITS_SIZE.load(), &SKULL_FRAME22),
@@ -229,7 +190,6 @@ fn main() -> ! {
             27 => (SKULL_FRAME27_BITS_SIZE.load(), &SKULL_FRAME27),
             28 => (SKULL_FRAME28_BITS_SIZE.load(), &SKULL_FRAME28),
             29 => (SKULL_FRAME29_BITS_SIZE.load(), &SKULL_FRAME29),
-
             _ => (SKULL_FRAME00_BITS_SIZE.load(), &SKULL_FRAME00),
         };
 
@@ -237,7 +197,7 @@ fn main() -> ! {
         arduino_hal::delay_ms(1);
     }
 
-    // Should be unreachable code if application work correct.
+    // Should be unreachable huffman_code if application work correct.
     loop {
         ufmt::uwriteln!(&mut serial, "Ping.").unwrap();
         arduino_hal::delay_ms(500);
